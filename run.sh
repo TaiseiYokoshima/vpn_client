0b457f6c9a4b822adc171003c5de50d0b25f3e3f
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2024-08-30 17:17:31 +0900

    can now receive ping packets

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ cargo b --release
 sudo setcap cap_net_admin=eip ./target/release/vpn_client 
 ./target/release/vpn_client &
 pid=$!
-sudo ip addr add 192.168.0.1/24 dev tun01
 sudo ip link set up dev tun01
+sudo ip addr add 192.168.0.1/24 dev tun01
+trap "kill $pid" INT TERM
 wait $pid
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use tun_tap::Mode;
 async fn main() -> std::io::Result<()> {
     let nic = Iface::new("tun01", Mode::Tun).expect("Could not create tun device");
     let mut buff = [0u8;1504];
-    let read = nic.recv(&mut buff[..])?;
-    eprintln!("read  {} bytes: {:x?}", read, &buff[..read]);
+    loop {
+        let read = nic.recv(&mut buff[..])?;
+        eprintln!("read  {} bytes: {:x?}", read, &buff[..read]);
+    }
     Ok(())
 }
